afc63ae1461a30cef8220ecf5e9d8001c4ef542d
dokma11 <vule.dok@gmail.com>
2023-04-10 09:11:56 +0200

    Created tour statistics view and viewmodel

---
+++ Sims2023/Sims2023/WPF/ViewModels/TourStatisticsViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Sims2023.Application.Services;
+using Sims2023.Domain.Models;
+using Sims2023.Observer;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Sims2023.WPF.ViewModels
+{
+    public partial class TourStatisticsViewModel : IObserver, INotifyPropertyChanged
+    {
+        private TourService _tourService;
+        public Tour? Tour { get; set; }
+        public Tour? SelectedTour { get; set; }
+        public Tour? TheMostVisitedTour { get; set; }
+        public ObservableCollection<Tour> ToursToDisplay { get; set; }
+        public User LoggedInGuide { get; set; }
+        public TourStatisticsViewModel(User loggedInGuide, TourService tourService)
+        {
+            _tourService = tourService;
+            LoggedInGuide = loggedInGuide;
+
+            DisplayTours();
+            DisplayTheMostVisitedTour();
+        }
+
+        private void DisplayTours()
+        {
+            foreach (var tour in _tourService.GetAll())
+            {
+                if (tour.CurrentState == Tour.State.Finished || tour.CurrentState == Tour.State.Interrupted)
+                {
+                    ToursToDisplay.Add(tour);
+                }
+            }
+        }
+
+        private void DisplayTheMostVisitedTour()
+        {
+
+        }
+
+        private void ShowStatisticsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void Update()
+        {
+            ToursToDisplay.Clear();
+            DisplayTours();
+        }
+    }
+}
