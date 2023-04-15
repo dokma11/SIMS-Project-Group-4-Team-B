1fdfa0a5c749cb3b5f34131d62aa97b2974d072d
1234marko123 <savicmarkoni@gmail.com>
2023-04-15 11:14:49 +0200

    Created RateTourViewModel and implemented for mvvm pattern

--- Sims2023/Sims2023/WPF/Views/Guest2Views/RateTourView.xaml.cs
+++ Sims2023/Sims2023/WPF/Views/Guest2Views/RateTourView.xaml.cs
@@ -12,55 +12,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Sims2023.Domain.Models;
-using Sims2023.Application.Services;
 using Sims2023.Observer;
+using Sims2023.WPF.ViewModels.Guest2ViewModels;
 
 namespace Sims2023.WPF.Views.Guest2Views
 {
-    /// <summary>
-    /// Interaction logic for RateTourView.xaml
-    /// </summary>
+   
     public partial class RateTourView : Window,IObserver
     {
         public User User { get; set; }
         public Tour Tour { get; set; }
-
-        public KeyPoint KeyPoint { get; set; }
-
-        public TourReviewService _tourReviewService;
-        public KeyPointService _keyPointService;
-        public TourReview TourReview { get; set; }
+        RateTourViewModel RateTourViewModel { get; set; }
         public RateTourView(User user,Tour tour)
         {
             InitializeComponent();
-            DataContext = this;
-
+           
             Tour = tour;
             User = user;
-             
-            //TourReview = new TourReview();
+            RateTourViewModel= new RateTourViewModel(user,tour,this);
 
-            _tourReviewService = new TourReviewService();
-            _tourReviewService.Subscribe(this);
-
-            _keyPointService = new KeyPointService();
-            _keyPointService.Subscribe(this);
-
-            KeyPoint = _keyPointService.GetById(0);//default and then guide will check at which KeyPoint has joined 
+            DataContext = RateTourViewModel;
         }
 
         private void Send_Click(object sender, RoutedEventArgs e)
         {
-             TourReview = new TourReview(User, Tour,KeyPoint, (int)guidesKnowledgeBox.Value, (int)tourInterestBox.Value, (int)guidesLanguageCapabilityBox.Value,(string)CommentTextBox.Text);
-            _tourReviewService.Create(TourReview);
-            MessageBox.Show("Hvala na recenziji");
-            Close();
-            
+            RateTourViewModel.Send_Click();
         }
 
         public void Update()
         {
-            
+            RateTourViewModel.Update();
         }
     }
 }
