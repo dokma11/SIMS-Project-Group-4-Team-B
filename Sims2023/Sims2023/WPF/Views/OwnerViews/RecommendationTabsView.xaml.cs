d644cb08beddb3b37a520348c3e579d5d84ee46b
Nedeljko Babic <bnedeljko0@gmail.com>
2023-06-05 13:42:43 +0200

    added funcionality to display a tutorial

--- Sims2023/Sims2023/WPF/Views/OwnerViews/RecommendationTabsView.xaml.cs
+++ Sims2023/Sims2023/WPF/Views/OwnerViews/RecommendationTabsView.xaml.cs
@@ -6,32 +6,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Sims2023.WPF.Views.OwnerViews
 {
-    /// <summary>
-    /// Interaction logic for RecommendationTabsView.xaml
-    /// </summary>
     public partial class RecommendationTabsView : Page
     {
-        
         public RecommendationTabsView()
         {
             InitializeComponent();
-            DataContext= this;
+            DataContext = this;
         }
 
         public void Popular_Click(object sender, RoutedEventArgs e)
         {
             PopularLocationsView popular = new PopularLocationsView();
             FrameManager.Instance.MainFrame.Navigate(popular);
+        }
+
+        private void PlayVideo_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Position = TimeSpan.Zero;
+            mediaElement.Play();
+        }
+
+        private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Stop();
+            mediaElement.Visibility = Visibility.Hidden;
         }
     }
 }
