a90c7327c9c21d3743157f8c8848268658a683b1
kirdatatjana <kirda.tatjana@gmail.com>
2023-06-06 14:44:49 +0200

    Backend basics made for Forum and ForumComment
    Model, repositories, services and file handlers made for Forum and ForumComment classes

---
+++ Sims2023/Sims2023/Domain/Models/ForumComment.cs
@@ -0,0 +1,55 @@
+﻿using Sims2023.Serialization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sims2023.Domain.Models
+{
+    public class ForumComment : ISerializable
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Forum Forum { get; set; }
+        public string Comment { get; set; }
+
+        public ForumComment() { }
+
+        public ForumComment(int id, User user, Forum forum, string comment)
+        {
+            Id = id;
+            User = user;
+            Forum = forum;
+            Comment = comment;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues =
+            {
+            Id.ToString(),
+            User.Id.ToString(),
+            Forum.Id.ToString(),
+            Comment
+        };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            User = new()
+            {
+                Id = Convert.ToInt32(values[1])
+            };
+            Forum = new()
+            {
+                Id = Convert.ToInt32(values[2])
+            };
+            Comment = values[3];
+        }
+    }
+
+}
---
+++ Sims2023/Sims2023/Domain/RepositoryInterfaces/IForumCVSRepository.cs
@@ -0,0 +1,25 @@
+﻿using Sims2023.Domain.Models;
+using Sims2023.Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sims2023.Domain.RepositoryInterfaces
+{
+    public interface IForumCSVRepository
+    {
+        Forum GetById(int id);
+        int NextId();
+        void Add(Forum forum);
+        void Remove(Forum forum);
+        void Save();
+        void Update(Forum forum);
+        List<Forum> GetAll();
+        void Subscribe(IObserver observer);
+        void Unsubscribe(IObserver observer);
+        void NotifyObservers();
+    }
+
+}
