cb6260026fa7a7dcddc6bdf2cd55ba1cb5f71aea
Nedeljko Babic <bnedeljko0@gmail.com>
2023-06-08 23:32:21 +0200

    fixed a bug where user could not click on a forum to open it

--- Sims2023/Sims2023/WPF/Views/OwnerViews/ListOfForumsView.xaml.cs
+++ Sims2023/Sims2023/WPF/Views/OwnerViews/ListOfForumsView.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using Sims2023.Application.Services;
 using Sims2023.Domain.Models;
+using Sims2023.WPF.ViewModels.OwnerViewModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,6 +29,8 @@ namespace Sims2023.WPF.Views.OwnerViews
         public LocationService _locationService;
         public AccommodationService _accommodationService;
         public ForumService _forumService;
+        public User user { get; set; }
+        public Forum SelectedForum { get; set; }
 
         public List<Location> locations { get; set; }
         public ObservableCollection<Forum> Forums { get; set; }
@@ -37,11 +41,20 @@ namespace Sims2023.WPF.Views.OwnerViews
             _locationService = new LocationService();
             _accommodationService = new AccommodationService();
             _forumService = new ForumService();
-
+            user = owner;
             locations = _accommodationService.GetOwnerLocations(_accommodationService.GetOwnerAccommodations(_accommodationService.GetAllAccommodations(), owner));
 
             Forums = new ObservableCollection<Forum>(_forumService.GetForumsForParticularOwner(locations));
             DataContext = this;
+        }
+
+        private void OpenForum_Click(object sender, RoutedEventArgs e)
+        {
+            if(SelectedForum !=null) 
+            {
+                CommentsView comments = new CommentsView(user, SelectedForum);
+                FrameManager.Instance.MainFrame.Navigate(comments);
+            }    
         }
     }
 }
